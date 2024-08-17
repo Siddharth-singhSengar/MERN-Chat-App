a33825546407d0bd2925310e1c1f5f97f5cf9a6e
Siddharth-singhSengar <156781416+Siddharth-singhSengar@users.noreply.github.com>
2024-08-17 20:31:29 +0530

    project setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Left from "./Home/Leftpart/Left"
+import Right from "./Home/Rightpart/Right"
+
+function hello() {
+  return (
+    <div className="flex h-screen">
+    <Left/>
+    <Right/>
+    </div>
+  )
+}
+export default hello
